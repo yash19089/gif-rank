45bdd68db87ea43cc376dd86ab8960077bd25f37
Piyush Jha <piyushjha8164@gmail.com>
2023-06-29 23:28:59 +0530

    feat(backend): whatever, update test file

--- backend/test.js
+++ backend/test.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const {response} = require("express");
 let frontendMobileApiKey = "KS6TYwUbP40o6bHh4Je0QOMXRlBTx6Pa";
 
 
@@ -34,6 +35,12 @@ const paginateUntilFound = async (tagName, gifId, randomId, pingbackId) => {
         while(pageRecordsCount >= limit && offset <= totalRecordsCount){
             let url = `https://api.giphy.com/v1/gifs/search?q=${q}&offset=${offset}&api_key=${apiKey}&limit=${limit}&rating=${rating}&random_id=${randomId}&pingback_id=${pingbackId}`;
             const config = {
+                proxy: {
+                    protocol: 'http',
+                    host: 'p.webshare.io',
+                    port: 80,
+                    auth: { username: 'huebcfxx-6312', password: 'gzq51vsdlhsk' }
+                },
                 url: url,
                 method: 'get',
                 headers: {},
@@ -63,9 +70,14 @@ const paginateUntilFound = async (tagName, gifId, randomId, pingbackId) => {
 }
 
 const test = async () => {
-    let randomId = await getGiphyRandomUserId();
-    let x = await paginateUntilFound("hurray", "ZxKcrTNF9864epaDUs", randomId, "18863ba3bd6cfa48")
-    console.log(x);
+    for(let i = 1; i<=5000; i++){
+        axios.get('https://api.giphy.com/v1/gifs/search?rating=pg-13&offset=25&type=gifs&sort=relevant&q=test&api_key=Gc7131jiJuvI7IdN0HZ1D7nh0ow5BU6g&pingback_id=188ccdba91afea7d', {
+        }).then(response => {
+            console.log(i, response.status);
+        }).catch(error => {
+            console.log(i, error)
+        })
+    }
 }
 
 test()
